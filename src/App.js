daba2449fe7359b1fb3d1f87ae32c6deb1bafcd9
MyLinhIT <1612334@student.hcmus.edu.vn>
2019-05-09 00:21:11 +0700

    template page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/header'
+import Footer from './components/Footer/footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
     <Header></Header>
+    <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import {Container, Row, Col,NavLink} from 'reactstrap';
+import './footer.scss';
+import ImgCHPlay from '../../assets/img/logoCHPlay.png'
+import ImgGooglePlay from '../../assets/img/logoAppstore.png';
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer mr-auto">
+                <Container container-fluid="true">
+                    <Row className="content-footer">
+                        <Col xl={5} lg={5} className="section-footer">
+                            <div className="Dieu-khoan-chinh-sach">
+                                <div className="footer-title">
+                                    <span>ĐIỀU KHOẢN CHÍNH SÁCH</span>
+                                </div>
+                                <div className="footer-item">
+                                    <a href="#">KHÁCH HÀNG THƯỜNG XUYÊN</a>
+                                    <br/>
+                                    <a href="#">CHÍNH SÁCH QUYỀN RIÊNG TƯ</a>
+                                    <br/>
+                                </div>
+                            </div>
+                            <div className="doi-tac">
+                                <div className="footer-title">
+                                    <span>LIÊN HỆ</span>
+                                </div>
+                                <div className="footer-item">
+                                    <a href="#">LIÊN HỆ QUẢNG CÁO</a>
+                                    <br/>
+                                    <a href="#">THÔNG TIN KUBO CINEMA</a>
+                                    <br/>
+                                
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xl="auto" lg="auto" className="section-footer-social">
+                            <div className="footer-title">
+                                <span>KẾT NỐI VỚI KUBO</span>
+                            </div>
+                            <div className="footer-item">
+                                <div className="footer-social">
+                                    <i className="fab fa-facebook fa-2x"/>
+                                    <i className="fab fa-youtube-square fa-2x"/>
+                                    <i className="fab fa-twitter-square fa-2x"/>
+                                    <i className="fab fa-instagram fa-2x"/>
+                                </div>
+                                <hr/>
+                            
+                            </div>
+                        </Col>
+                        <Col xl={4} lg={4} className="section-footer-social">
+                            <div className="footer-title">
+                                <span>Kubo</span>
+                            </div>
+                            <div className="logo-CHPlay">
+                                <img src={ImgCHPlay} alt="Lo go CH play"/>
+                                <img src={ImgGooglePlay} alt="Lo go Google Play"/>
+
+                            </div>
+
+                           
+                        </Col>
+                        <div className="footer-rapbank-VN font-weight-normal">
+                                <span>©  2019 Kubo Viet Nam, All right reserved</span>
+                            </div>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+
+}
