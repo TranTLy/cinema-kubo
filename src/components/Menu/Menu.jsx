c27a7db55e93bccd013b1db9ba0f0022895f63be
MyLinhIT <1612334@student.hcmus.edu.vn>
2019-05-10 23:53:46 +0700

    rename header -> menu, add component header contain button login signin

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem, Container, Row, Form, Input, InputGroup, InputGroupAddon
+} from 'reactstrap';
+import './Menu.scss'
+
+export default class Menu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div className="menu sticky-top">
+                <Container>
+                    <Row>
+                        <Navbar expand="md" className="w-100">
+                            <NavbarToggler onClick={this.toggle} />
+                            <Collapse isOpen={this.state.isOpen} navbar>
+                                <Nav navbar>
+                                    <NavItem>
+                                        <NavLink href="#">Trang chủ</NavLink>
+                                    </NavItem>
+
+                                    <UncontrolledDropdown nav inNavbar>
+                                        <DropdownToggle nav caret>
+                                            Thể loại
+                                        </DropdownToggle>
+                                        <DropdownMenu right>
+                                            <DropdownItem>
+                                                Tâm lý - tình cảm
+                                            </DropdownItem>
+                                            <DropdownItem>
+                                                Kinh dị
+                                            </DropdownItem>
+                                            <DropdownItem />
+                                            <DropdownItem>
+                                                Hài
+                                            </DropdownItem>
+                                        </DropdownMenu>
+                                    </UncontrolledDropdown>
+                                    <NavItem>
+                                        <NavLink href="#">Giới thiệu</NavLink>
+                                    </NavItem>
+                                </Nav>
+                                <Form className="navbar-search d-none d-md-flex d-lg-flex ml-auto mr-4">
+                                    <InputGroup className="ml-0">
+                                        <Input placeholder="Tìm kiếm..." type="text" className="form-control" />
+                                        <InputGroupAddon addonType="append" className="pt-1 pr-1">
+                                        <a href="#">
+                                        <i class="material-icons">
+                                            search
+                                    </i></a></InputGroupAddon>
+
+                                    </InputGroup>
+                                </Form>
+                            </Collapse>
+                        </Navbar>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
---
+++ src/components/PageLogIn_SignIn_Intro/Page_LogIn_SignIn_Intro.jsx
@@ -0,0 +1,9 @@
+import React, {Component } from 'react';
+import LogIn from './LogIn/LogIn'
+export default class PageLogInSignInIntro extends Component{
+    render() {
+        return (
+             <LogIn></LogIn>
+        );
+    }
+}
