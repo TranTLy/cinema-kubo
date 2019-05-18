20d35630c5f2aa9dd0559067bfa123af792684fb
TranLy <1612367@student.hcmus.edu.vn>
2019-05-18 09:24:43 +0700

    Sua react-scripts tu 3.0.0 thanh 2.1.15

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
-import PageLogIn_SignIn_Intro from "./components/PageLogIn_SignIn_Intro/Page_LogIn_SignIn_Intro";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import PageLogIn_SignIn_Intro from './components/PageLogIn_SignIn_Intro/Page_LogIn_SignIn_Intro';
+import Home from './containers/Home/Home';
 const routing = (
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/Login" component={PageLogIn_SignIn_Intro} />
-    </div>
-  </Router>
+	<Router>
+		<div>
+			<Route exact path="/" component={App} />
+			<Route path="/Login" component={PageLogIn_SignIn_Intro} />
+			<Route path="/home" component={Home} />
+		</div>
+	</Router>
 );
-ReactDOM.render(routing, document.getElementById("root"));
+ReactDOM.render(routing, document.getElementById('root'));
 serviceWorker.unregister();
