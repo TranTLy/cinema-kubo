13882aadb7ab86d10387d966d06c5ec7adf6371e
MyLinhIT <1612334@student.hcmus.edu.vn>
2019-05-18 22:23:07 +0700

    Reset lại merger từ lý vào master

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MostFamousMovie from "./components/MostFamousMovie/MostFamousMovie";
 import ComedyMovie from "./components/ComedyMovie/ComedyMovie";
 import RomanticMovie from "./components/RomanticMovie/RomanticMovie";
 import Poster from "./components/Poster/Poster";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/header";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "reactstrap";
@@ -13,8 +13,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Menu></Menu>
-        <Poster />
+      <Menu />
+      <Poster />
       <Container className="pt-4">
         <MostFamousMovie />
       </Container>
